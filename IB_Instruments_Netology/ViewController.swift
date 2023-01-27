125a359b720bca3750a06591d1655bab6b59cdfc
Sergey <tomyettimcneal@gmail.com>
2023-01-27 13:58:25 +0300

    first commit

---
+++ IB_Instruments_Netology/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IB_Instruments_Netology
+//
+//  Created by My Air on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
